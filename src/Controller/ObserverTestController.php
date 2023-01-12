3b7f281b5ca514e18e8d7baa112c056519d6ce40
AlexC2103 <alexandrucioi@gmail.com>
2023-01-12 23:50:57 +0200

    desktop

--- src/Controller/ObserverTestController.php
+++ src/Controller/ObserverTestController.php
@@ -5,22 +5,23 @@ namespace App\Controller;
 use App\Helpers\Observer\EventExporter;
 use App\Helpers\EventManager;
 use App\Helpers\Observer\User;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ObserverTestController
+class ObserverTestController extends AbstractController
 {
-    public function observerTest(): Response {
+    public function observerTest()
+    {
         $eventManager = EventManager::getInstance();
         $user1 = new User('Alex', 'alex@gmail.com');
         $user2 = new User('Florin', 'florin@gmail.com');
         $user3 = new User('Veta', 'veta@gmail.com');
 
-        $publisher = new EventExporter();
+        $publisher1 = new EventExporter();
 
-        $eventManager->attach($user1, $publisher->exportEvent('eveniment1'));
+        $eventManager->attach($user1, $publisher1->exportEvent('EvenimentPublisher1'));
+        $eventManager->attach($user2, $publisher1->exportEvent('EvenimentPublisher1'));
+        $eventManager->attach($user3, $publisher1->exportEvent('EvenimentPublisher1'));
 
-        return new Response(
-
-        );
+        return $this->render('observerTemplates/observer.twig.html');
     }
 }
