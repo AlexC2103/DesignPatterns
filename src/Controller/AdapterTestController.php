aac23f80c2de7c7dc3315f5500ac49d59ed0d3c2
AlexC21 <alexandrucioi@gmail.com>
2023-02-13 18:20:09 +0200

    laptop

--- src/Controller/AdapterTestController.php
+++ src/Controller/AdapterTestController.php
@@ -3,15 +3,23 @@
 namespace App\Controller;
 
 use App\Helpers\Adapter\NotificationTypes\EmailNotification;
+use App\Helpers\Adapter\NotificationTypes\SlackNotification;
+use App\Helpers\Adapter\Notification;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdapterTestController
 {
     public function adapterTest(): Response
     {
-        $notification = new EmailNotification("developer@example.com");
+        function clientCode(Notification $notification)
+        {
+            echo $notification->send("Website is down!",
+                "<strong style='color:red;font-size: 50px;'>Alert!</strong>" .
+                "Our website is not responding. Call admins and bring it up!");
+        }
 
-
+        $notification = new EmailNotification("admin@gmail.com");
+        clientCode($notification);
 
         return new Response (
 
